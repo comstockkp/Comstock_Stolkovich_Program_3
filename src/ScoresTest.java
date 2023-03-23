f8cae793a008e6f9c2d0d2f10255ca05a121ef41
COMSTOCKKP22 <comstockkp22@gcc.edu>
2023-03-23 13:45:00 -0400

    Initial commit

---
+++ src/ScoresTest.java
@@ -0,0 +1,16 @@
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ScoresTest {
+    /*
+     * Constructor tests using numScores() and get()
+     */
+    
+    /*
+     * getMax() tests
+     */
+    
+}
